e4e99f0647a8003974f903690b13b30318cc722a
lutokozima <61746936+lutokozima@users.noreply.github.com>
2023-10-18 10:08:05 -0300

    Update criar_pedido.sql

--- criar_pedido.sql
+++ criar_pedido.sql
@@ -6,14 +6,14 @@ data_pedido IN DATE
 )
 IS
     qtd_estoque NUMBER;
-	preco_produto NUMBER;
+    preco_produto NUMBER;
 	
 BEGIN
     -- Verifica se o produto está em estoque na quantidade desejada
     SELECT quantidade_estoque, preco 
-	INTO qtd_estoque, preco_produto
-	FROM produtos 
-	WHERE id = idProduto;
+      INTO qtd_estoque, preco_produto
+      FROM produtos 
+     WHERE id = idProduto;
     
     IF qtd_estoque >= quantidade THEN
 
@@ -23,18 +23,18 @@ BEGIN
         
         -- Atualiza o estoque do produto:
         UPDATE produtos 
-		   SET quantidade_estoque = qtd_estoque - quantidade 
-		 WHERE id = idProduto;
+	   SET quantidade_estoque = qtd_estoque - quantidade 
+	 WHERE id = idProduto;
         
         DBMS_OUTPUT.PUT_LINE('Pedido criado com sucesso!');
-		COMMIT;
+	COMMIT;
     ELSE
         DBMS_OUTPUT.PUT_LINE('Produto fora de estoque.');
     END IF;
 
-    EXCEPTION
-		WHEN NO_DATA_FOUND THEN
-			DBMS_OUTPUT.PUT_LINE('Produto não existe'); 
-        WHEN OTHERS THEN 
-            DBMS_OUTPUT.PUT_LINE('Erro: ' || SQLCODE || ' - ' || SQLERRM); 
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+	 DBMS_OUTPUT.PUT_LINE('Produto não existe'); 
+    WHEN OTHERS THEN 
+         DBMS_OUTPUT.PUT_LINE('Erro: ' || SQLCODE || ' - ' || SQLERRM); 
 END;
