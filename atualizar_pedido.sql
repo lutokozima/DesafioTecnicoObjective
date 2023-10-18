95c916105a20062e7c0c14ec54b1d54f230d3844
lutokozima <61746936+lutokozima@users.noreply.github.com>
2023-10-18 10:04:57 -0300

    Update atualizar_pedido.sql

--- atualizar_pedido.sql
+++ atualizar_pedido.sql
@@ -10,34 +10,33 @@ IS
 BEGIN
     -- Verifica o estoque do produto 
     SELECT quantidade_estoque, preco 
-	  INTO qtd_estoque, preco 
-	  FROM produtos 
-	 WHERE id = idProduto;
+      INTO qtd_estoque, preco 
+      FROM produtos 
+     WHERE id = idProduto;
     
 	--verifica se a o produto em estoque atende a nova quantidade desejada
     IF qtd_estoque >= nova_qtd THEN
         -- Atualiza os detalhes do pedido na tabela Pedidos:
         UPDATE pedidos 
-	   	   SET id_produto = idProduto, 
-			   quantidade = nova_qtd, 
-			   data_pedido = nova_data_pedido 
-		 WHERE id = idPedido;
+  	   SET id_produto = idProduto, 
+  	       quantidade = nova_qtd, 
+	       data_pedido = nova_data_pedido 
+	 WHERE id = idPedido;
         
         -- Refletir as alterações no estoque do produto
         UPDATE produtos 
-		   SET quantidade_estoque = qtd_estoque - nova_qtd 
-		 WHERE id = idProduto;
+	   SET quantidade_estoque = qtd_estoque - nova_qtd 
+	 WHERE id = idProduto;
         
         DBMS_OUTPUT.PUT_LINE('Pedido atualizado com sucesso!');
-		COMMIT;
+	COMMIT;
     ELSE
         DBMS_OUTPUT.PUT_LINE('Produto não tem estoque suficiente.');
     END IF;
     
-    EXCEPTION 
-		WHEN NO_DATA_FOUND THEN
-			DBMS_OUTPUT.PUT_LINE('Produto não existe'); 
-		
-        WHEN OTHERS THEN 
-            DBMS_OUTPUT.PUT_LINE('Erro: ' || SQLCODE || ' - ' || SQLERRM); 
+EXCEPTION 
+    WHEN NO_DATA_FOUND THEN
+	 DBMS_OUTPUT.PUT_LINE('Produto não existe'); 
+    WHEN OTHERS THEN 
+         DBMS_OUTPUT.PUT_LINE('Erro: ' || SQLCODE || ' - ' || SQLERRM); 
 END;
